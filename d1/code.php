8ebecf472990f748097b45d1b429babc5f6461de
grospe-faye <fayebheevayne@gmail.com>
2022-03-07 18:49:41 +0800

    initial commit

---
+++ d1/code.php
@@ -0,0 +1,14 @@
+<?php
+// PHP Comments
+// Single line
+/*
+	Multi line
+*/
+
+// Variables - used to store values or contain data
+// Variables in PHP are defined using the dollar notation before the name of the variable.
+
+
+$name = 'John Smith';
+$email = 'johnsmith@mail.com';
+
---
+++ d1/index.php
@@ -0,0 +1,18 @@
+<?php require_once "./code.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>S01: PHP Basics and Selection Control Structures</title>
+</head>
+<body>
+	<h1>Echoing Values</h1>
+
+	<!-- Variables cannot be used in single quotes -->
+	<p><?php echo 'Hello Batch 168' ?></p>
+	<p><?php echo 'Good day $name! Your given email is $email'; ?></p>
+	<p><?php echo "Good day $name! Your given email is $email"; ?></p>
+</body>
+</html>
