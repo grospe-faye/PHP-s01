27ac7424696711453732b9028d5fa8659d8d3781
grospe-faye <fayebheevayne@gmail.com>
2022-03-07 20:46:04 +0800

    Update with Operators

--- d1/code.php
+++ d1/code.php
@@ -7,8 +7,67 @@
 
 // Variables - used to store values or contain data
 // Variables in PHP are defined using the dollar notation before the name of the variable.
-
-
 $name = 'John Smith';
 $email = 'johnsmith@mail.com';
+
+// Constants
+// Defined using the define() function
+define('PI', 3.1415);
+
+// Data Types
+
+// String
+$state = 'New York';
+$country = 'United States of America';
+$address = $state.', '.$country; //Concatenation via dot notation
+$addressTwo = "$state, $country"; //Concatenation cia double quotes
+
+// Integers
+$age = 31;
+$headcount = 26;
+
+// Floats
+$grade = 98.2;
+$distanceInKm = 2562.23;
+
+// Boolean
+$hasTravelledAbroad  = false;
+$haveSymptoms = true;
+
+// Null
+$spouse = null;
+$middleName = null;
+
+// Array
+$grades = array(98.7, 92.1, 90.2, 94.6);
+
+// Objects
+$gradesObj = (object)[
+	'firstGrading' => 98.7,
+	'secondGrading' => 92.1,
+	'thirdGrading' => 94.6,
+	'fourthGrading' => 90.2
+];
+
+$personObj = (object)[
+	'fullName' => 'John Smith',
+	'isMarried' => false,
+	'age' => 35,
+	'address' => (object)[
+		'state' => 'Washington DC',
+		'country' => 'United States of America'
+	]
+];
+
+// Operators
+
+// Assignment Operators
+$x = 56.2;
+$y = 912.6;
+
+$isLegalAge = true;
+$isRegistered = false;
+
+
+
 
--- d1/index.php
+++ d1/index.php
@@ -11,8 +11,54 @@
 	<h1>Echoing Values</h1>
 
 	<!-- Variables cannot be used in single quotes -->
+
 	<p><?php echo 'Hello Batch 168' ?></p>
 	<p><?php echo 'Good day $name! Your given email is $email'; ?></p>
 	<p><?php echo "Good day $name! Your given email is $email"; ?></p>
+	<p><?php echo PI; ?></p>
+
+	<!-- Data Types -->
+
+	<p><?php echo "State is $state in $country"; ?> </p>
+	<p><?php echo $address; ?> </p>
+	<p><?php echo $addressTwo; ?> </p>
+	<p><?php echo $age; ?> </p>
+
+	<!-- Normal Echoing of Boolean and Null Variables will not appear in the web output-->
+
+	<p><?php echo $hasTravelledAbroad; ?></p>
+	<p><?php echo $spouse; ?></p>
+
+	<!-- gettype function return the type of a variable -->
+	<p><?php echo gettype($hasTravelledAbroad); ?></p>
+	<!-- var_dump gives detail about the variable -->
+	<p><?php echo var_dump($spouse); ?></p>
+
+	<p><?php echo $gradesObj->firstGrading; ?></p>
+	<p><?php echo $personObj->address->state; ?></p>
+
+	<p><?php echo $grades[3]; ?></p>
+	<p><?php echo $grades[0]; ?></p>
+
+	<h1>Operators</h1>
+	<p> X: <?php echo $x; ?></p>
+	<p> y: <?php echo $y; ?></p>
+
+	<p>Is Legal Age: <?php echo var_dump($isLegalAge); ?></p>
+	<p>Is Registered Age: <?php echo var_dump($isRegistered); ?></p>
+
+	<h2>Arithmetic Operators</h2>
+	<p>Sum: <?php echo $x + $y ?></p>
+	<p>Difference: <?php echo $x - $y ?></p>
+	<p>Product: <?php echo $x * $y ?></p>
+	<p>Quotient: <?php echo $x / $y ?></p>
+
+	<h2>Equality Operators</h2>
+
+	<p>Loose Equality: <?php echo var_dump($x == '56.2') ?></p>
+	<p>Strict Equality: <?php echo var_dump($x === '56.2') ?></p>
+	<p>Loose Equality: <?php echo var_dump($x != '56.2') ?></p>
+	<p>Strict Equality: <?php echo var_dump($x !== '56.2') ?></p>
+
 </body>
 </html>
